038daa6d081a44a4e5f2d35ff67eeede4711426a
Feliz Gubitz <feliz@trbc.io>
2018-10-18 18:49:51 +0200

    default project tarifacash

--- firetestagain/src/firebase.js
+++ firetestagain/src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase'
 
 const app = initializeApp({
-  apiKey: "AIzaSyCiBdZln9wlNoE0vPathY1uWxDIh9o-lsA",
-  authDomain: "tarifacash.firebaseapp.com",
-  databaseURL: "https://tarifacash.firebaseio.com",
-  projectId: "tarifacash",
-  storageBucket: "tarifacash.appspot.com",
-  messagingSenderId: "225078073600"
+  apiKey: 'AIzaSyCiBdZln9wlNoE0vPathY1uWxDIh9o-lsA',
+  authDomain: 'tarifacash.firebaseapp.com',
+  databaseURL: 'https://tarifacash.firebaseio.com',
+  projectId: 'tarifacash',
+  storageBucket: 'tarifacash.appspot.com',
+  messagingSenderId: '225078073600'
 })
 
 export const db = app.database()
