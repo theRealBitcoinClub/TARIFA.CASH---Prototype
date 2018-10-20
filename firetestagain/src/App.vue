c5b5df4299d8d774b651839fcb429f665f3fb342
Feliz Gubitz <feliz@trbc.io>
2018-10-20 21:06:02 +0200

    use metainfo as a function

--- firetestagain/src/App.vue
+++ firetestagain/src/App.vue
@@ -10,43 +10,50 @@
 <script>
 export default {
   name: 'App',
-  metaInfo: {
-    links: [
-      {rel: 'canonical', href: 'https://www.my-site.com/my-special-page'}
-    ],
-    // if no subcomponents specify a metaInfo.title, this title will be used
-    title: 'TRBC - Tarifa Cash',
-    // all titles will be injected into this template
-    titleTemplate: '%s | BCH Bitcoin',
-    meta: [
-      {'http-equiv': 'Content-Type', content: 'text/html; charset=utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'},
-      {name: 'description', content: 'Tarifa Cash is a local currency backed by BCH - Bitcoin Cash'},
-      // OpenGraph data (Most widely used)
-      {property: 'og:title', content: 'My Page Title ← My Site'},
-      {property: 'og:site_name', content: 'My Site'},
-      // The list of types is available here: http://ogp.me/#types
-      {property: 'og:type', content: 'website'},
-      // Should the the same as your canonical link, see below.
-      {property: 'og:url', content: 'https://www.my-site.com/my-special-page'},
-      {property: 'og:image', content: 'https://www.my-site.com/my-special-image.jpg'},
-      // Often the same as your meta description, but not always.
-      {property: 'og:description', content: 'I have things here on my site.'},
+  data () {
+    return {
+      url: 'https://tarifa.cash',
+      desc: 'Tarifa Cash is a local currency backed by BCH - Bitcoin Cash',
+      img: '/my-special-image.jpg',
+      title: 'TRBC - Tarifa Cash'
+    }
+  },
+  metaInfo () {
+    return {
+      links: [
+        {rel: 'canonical', href: this.url}
+      ],
+      // if no subcomponents specify a metaInfo.title, this title will be used
+      title: this.title,
+      // all titles will be injected into this template
+      titleTemplate: '%s | BCH Bitcoin',
+      meta: [
+        {'http-equiv': 'Content-Type', content: 'text/html; charset=utf-8'},
+        {name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'},
+        {name: 'description', content: this.desc},
 
-      // Twitter card
-      {name: 'twitter:card', content: 'summary'},
-      {name: 'twitter:site', content: 'https://www.my-site.com/my-special-page'},
-      {name: 'twitter:title', content: 'My Page Title ← My Site'},
-      {name: 'twitter:description', content: 'I have things here on my site.'},
-      // Your twitter handle, if you have one.
-      {name: 'twitter:creator', content: '@alligatorio'},
-      {name: 'twitter:image:src', content: 'https://www.my-site.com/my-special-image.jpg'},
+        // OpenGraph data (Most widely used)
+        {property: 'og:title', content: this.title},
+        {property: 'og:site_name', content: this.title},
+        {property: 'og:type', content: 'website'},
+        {property: 'og:url', content: this.url},
+        {property: 'og:image', content: this.url + this.img},
+        {property: 'og:description', content: this.desc},
 
-      // Google / Schema.org markup:
-      {itemprop: 'name', content: 'My Page Title My Site'},
-      {itemprop: 'description', content: 'I have things here on my site.'},
-      {itemprop: 'image', content: 'https://www.my-site.com/my-special-image.jpg'}
-    ]
+        // Twitter card
+        {name: 'twitter:card', content: this.desc},
+        {name: 'twitter:site', content: this.url},
+        {name: 'twitter:title', content: this.title},
+        {name: 'twitter:description', content: this.desc},
+        {name: 'twitter:creator', content: '@barriobitcoin'},
+        {name: 'twitter:image:src', content: this.url + this.img},
+
+        // Google / Schema.org markup:
+        {itemprop: 'name', content: this.title},
+        {itemprop: 'description', content: this.desc},
+        {itemprop: 'image', content: this.url + this.img}
+      ]
+    }
   }
 }
 </script>
